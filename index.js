4564635afb266a45ad25ec91e02e022014f5421b
Patrik Wibron <patrik@wibron.nu>
2016-06-16 19:32:50 +0200

    Add ESLint, upgrade Node and lint files

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-'use strict';
-var isWeekend = function (date) {
-  var dateString = date,
-      weekDay = dateString.getDay(),
-      hour = ((dateString.getHours() + 11) % 12 + 1);
+const isWeekend = date => {
+  const dateString = date;
+  const weekDay = dateString.getDay();
+  const hour = ((dateString.getHours() + 11) % 12 + 1);
   return ((weekDay === 5 && hour >= 5) || weekDay === 6 || weekDay === 0);
 };
 
-var ere = function (date) {
+const ere = (date = new Date()) => {
   if (!date instanceof Date) {
     throw new Error('Invalid date format. Use new Date()');
   }
-  date = date || new Date();
+
   return isWeekend(date);
 };
 
-exports.ere =  ere;
+exports.ere = ere;
--- test.js
+++ test.js
@@ -1,66 +1,57 @@
-'use strict';
-var assert = require('assert'),
-    timeKeeper = require('timekeeper'),
-    helg = require('./');
+const assert = require('assert');
+const timeKeeper = require('timekeeper');
+const helg = require('./');
 
-var wednesday = new Date(1414580400000),
-    saturday = new Date(1414839600000),
-    friday = new Date(1416600000000),
-    sunday = new Date(1414926000000);
+const wednesday = new Date(1414580400000);
+const saturday = new Date(1414839600000);
+const friday = new Date(1416600000000);
+const sunday = new Date(1414926000000);
 
-describe('Helg', function () {
-
-  describe('When passing an erroneous date object', function () {
-    it('should throw an error', function () {
-      var invalid = function () {
-        return helg.ere('Sunday 11th 2014');
-      };
+describe('Helg', () => {
+  describe('When passing an erroneous date object', () => {
+    it('should throw an error', () => {
+      const invalid = () => helg.ere('Sunday 11th 2014');
       assert.throws(invalid, Error);
     });
-  })
+  });
 
-  describe('When setting the date manually', function () {
-
-    it('should return false when the day is wednesday', function () {
+  describe('When setting the date manually', () => {
+    it('should return false when the day is wednesday', () => {
       assert.equal(helg.ere(wednesday), false);
     });
 
-    it('should return true when the day is saturday', function () {
+    it('should return true when the day is saturday', () => {
       assert.equal(helg.ere(saturday), true);
     });
 
-    it('should return true when the day is sunday', function () {
+    it('should return true when the day is sunday', () => {
       assert.equal(helg.ere(sunday), true);
     });
 
-    it('should return true when the day is friday and after 5pm', function () {
+    it('should return true when the day is friday and after 5pm', () => {
       assert.equal(helg.ere(friday), true);
     });
-
   });
 
-  describe('When getting the date from the computers local time', function () {
-
-    it('should return true when the day is saturday', function () {
+  describe('When getting the date from the computers local time', () => {
+    it('should return true when the day is saturday', () => {
       timeKeeper.travel(saturday);
       assert.equal(helg.ere(), true);
     });
 
-    it('should return false when the day is wednesday', function () {
+    it('should return false when the day is wednesday', () => {
       timeKeeper.travel(wednesday);
       assert.equal(helg.ere(), false);
     });
 
-    it('should return true when the day is friday and after 5pm', function () {
+    it('should return true when the day is friday and after 5pm', () => {
       timeKeeper.travel(friday);
       assert.equal(helg.ere(), true);
     });
 
-    it('should return true when the day is sunday', function () {
+    it('should return true when the day is sunday', () => {
       timeKeeper.travel(sunday);
       assert.equal(helg.ere(), true);
     });
-
   });
-
 });
